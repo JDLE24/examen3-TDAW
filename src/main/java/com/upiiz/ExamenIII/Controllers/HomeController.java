691e620f30e1c18ebb15f280d6c1eedf6ce3aaee
JDLE24 <145094976+JDLE24@users.noreply.github.com>
2025-06-24 03:17:07 -0600

    Update HomeController.java

--- src/main/java/com/upiiz/ExamenIII/Controllers/HomeController.java
+++ src/main/java/com/upiiz/ExamenIII/Controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "¡Bienvenido! La API está en funcionamiento.";
+    public String mostrarInicio() {
+        return "index";
     }
 }
